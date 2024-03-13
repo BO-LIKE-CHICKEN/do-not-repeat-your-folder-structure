43710dc3a8c23ec685de88363d9dc6601411258c
bosung baek <teabs1125@gmail.com>
2024-03-13 20:47:26 +0900

    feat: 컴포넌트 파일명 변경, export 방식 변경, 테스트 파일 추가

--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,51 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
+import { generateComponentTemplate, generateIndexTemplate } from "./template";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
+  const disposable = vscode.commands.registerCommand(
     "dryfs.makeMyFolder",
     (uri: vscode.Uri) => {
-      let folderPath: string;
-      if (fs.lstatSync(uri.fsPath).isDirectory()) {
-        // 선택한 리소스가 폴더일 때
-        folderPath = uri.fsPath;
-      } else {
-        // 선택한 리소스가 파일일 때
-        folderPath = path.dirname(uri.fsPath);
-      }
-
-      // React 컴포넌트 템플릿 생성 함수
-      function generateComponentTemplate(folderName: string) {
-        return `import React from 'react';
-
-type Props = {};
-
-const ${folderName} = (props: Props) => {
-  return <div>${folderName}</div>;
-};
-
-export default ${folderName};
-`;
-      }
+      const folderPath = fs.lstatSync(uri.fsPath).isDirectory()
+        ? uri.fsPath
+        : path.dirname(uri.fsPath);
 
       // 사용자로부터 폴더 이름을 입력 받음
       vscode.window
         .showInputBox({ prompt: "Enter the folder name" })
         .then((folderName) => {
           if (folderName) {
-            let newFolderPath = path.join(folderPath, folderName);
+            const newFolderPath = path.join(folderPath, folderName);
 
             // 폴더 생성
             if (!fs.existsSync(newFolderPath)) {
               fs.mkdirSync(newFolderPath);
             }
 
-            // index.tsx 파일에 React 컴포넌트 코드 작성
+            // ${folderName}.tsx 파일에 React 컴포넌트 코드 작성
             const componentTemplate = generateComponentTemplate(folderName);
+
             fs.writeFileSync(
-              path.join(newFolderPath, "index.tsx"),
+              path.join(newFolderPath, `${folderName}.tsx`),
               componentTemplate
+            );
+
+            const indexTemplate = generateIndexTemplate(folderName);
+            // index.ts 파일에서 React 컴포넌트 export
+            fs.writeFileSync(
+              path.join(newFolderPath, "index.ts"),
+              indexTemplate
             );
 
             // styles.ts 파일 생성
             fs.writeFileSync(path.join(newFolderPath, "styles.ts"), "");
+
+            // ${folderName}.test.tsx 파일 생성
+            fs.writeFileSync(
+              path.join(newFolderPath, `${folderName}.test.tsx`),
+              ""
+            );
 
             vscode.window.showInformationMessage(
               "Folder and files created successfully!"
